f34e2dbd0262f32222ee821aba0171d63cbe7165
omecity <olumeiza@yahoo.com>
2025-01-03 13:25:04 -0500

    added a step method that walks through the time_step and define the reward as change in the portfolio_value

--- TradingEnv.py
+++ TradingEnv.py
@@ -25,3 +25,23 @@ class TradingEnvironment:
         self.holdings = 0
         self.done = False
         return self.get_state()
+
+    def step(self, action):
+        # Actions: 0 = Hold, 1 = Buy, 2 = Sell
+        current_price = self.data.iloc[self.current_step]['Close']
+        if action == 1:
+            shares_to_buy = self.balance // current_price
+            self.holdings += shares_to_buy
+            self.balance -= shares_to_buy * current_price
+        elif action == 2: 
+            self.balance += self.holdings * current_price
+            self.holdings = 0
+
+        self.current_step += 1
+        if self.current_step >= self.n_steps - 1:
+            self.done = True
+
+        self.portfolio_value = self.balance + (self.holdings * current_price)
+        # Reward is defined as the change in portfolio value
+        reward = self.portfolio_value - 10000  
+        return self.get_state(), reward, self.done
