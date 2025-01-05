d712bc6069ae7201399edcce4bb4ab56d476cd37
omecity <olumeiza@yahoo.com>
2025-01-04 20:48:14 -0500

    added a file that contains a DQN Model

---
+++ TradingModel.py
@@ -0,0 +1,15 @@
+
+
+class DQN(nn.Module):
+    def __init__(self, input_dim, action_dim):
+        super(DQN, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(input_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, action_dim),
+        )
+
+    def forward(self, x):
+        return self.fc(x)
