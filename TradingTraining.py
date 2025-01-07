45ecb8ff01853e26d67c10e66a3691affd80adfe
omecity <olumeiza@yahoo.com>
2025-01-07 15:24:09 -0500

    created an environment and an agent

--- TradingTraining.py
+++ TradingTraining.py
@@ -2,3 +2,5 @@
 
 def train_agent(ticker="AAPL", start_date="2018-01-01", end_date="2020-01-01", episodes=50):
     data = fetch_stock_data(ticker, start_date, end_date)
+    env = TradingEnvironment(data)
+    agent = DQNAgent(state_dim=3, action_dim=3)
